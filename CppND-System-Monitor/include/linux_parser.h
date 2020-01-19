b0d5f7e06ea589beeca6b452808b4130a3790fe5
Christian Leininger <christianleininger42@gmail.com>
2020-01-20 00:05:36 +0100

    add function for memory

--- CppND-System-Monitor/include/linux_parser.h
+++ CppND-System-Monitor/include/linux_parser.h
@@ -52,6 +52,12 @@ std::string Ram(int pid);
 std::string Uid(int pid);
 std::string User(int pid);
 long int UpTime(int pid);
+
+
+// Memory
+float getMemoryUtilization();
+
+
 };  // namespace LinuxParser
 
 #endif
