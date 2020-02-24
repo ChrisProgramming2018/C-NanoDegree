0b0e002fecb1f45770bcca6b8bb470ebddee9514
Christian Leininger <christianleininger42@gmail.com>
2020-02-24 22:02:17 +0100

    checkstyle

--- CppND-System-Monitor/src/main.cpp
+++ CppND-System-Monitor/src/main.cpp
@@ -1,5 +1,10 @@
-#include "ncurses_display.h"
-#include "system.h"
+// Copyright 2020
+// // Udacity Project
+// // Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#include "../include/ncurses_display.h"
+#include "../include/system.h"
 
 int main() {
   System system;
