6b96c4014b9ad841c0e2f575129f390119afeb2f
Christian Leininger <christianleininger42@gmail.com>
2020-02-24 22:00:47 +0100

    checkstyle

--- CppND-System-Monitor/src/format.cpp
+++ CppND-System-Monitor/src/format.cpp
@@ -1,21 +1,25 @@
-#include <string>
+// Copyright 2020
+// // Udacity Project
+// // Author: Christian Leininger <info2016frei@gmail.com>
 
-#include "format.h"
+
+#include <string>
+#include "../include/format.h"
 
 using std::string;
 
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // __________________________________________________________________________________________________________________________________________________
-string Format::ElapsedTime(long seconds) { 
+string Format::ElapsedTime(int64_t seconds) {
   int hour = seconds / 3600;
-  int min = (seconds / 60) % 60 ;
+  int min = (seconds / 60) % 60;
   int sec = seconds % 60;
   string pre_hour = "";
   string pre_min = "";
   string pre_sec = "";
   if (sec < 10) { pre_sec = "0";}
   if (min < 10) {pre_min = "0";}
-  if (hour < 10){ pre_hour = "0";}
-  return  pre_hour + std::to_string(hour) + ":" + pre_min + std::to_string(min) + ":" + pre_sec + std::to_string(sec);
+  if (hour < 10) { pre_hour = "0";}
+  return pre_hour + std::to_string(hour) + ":" + pre_min + std::to_string(min) + ":" + pre_sec + std::to_string(sec);
 }
