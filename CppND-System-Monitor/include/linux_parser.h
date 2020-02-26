b237d6e83107d3098045313b7e0faf49fd422bac
Christian Leininger <christianleininger42@gmail.com>
2020-02-26 17:54:52 +0100

    checkstyle

--- CppND-System-Monitor/include/linux_parser.h
+++ CppND-System-Monitor/include/linux_parser.h
@@ -1,64 +1,74 @@
-#ifndef SYSTEM_PARSER_H
-#define SYSTEM_PARSER_H
+// Copyright 2020
+// Udacity Project
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+
+#ifndef CPPND_SYSTEM_MONITOR_INCLUDE_LINUX_PARSER_H_
+#define CPPND_SYSTEM_MONITOR_INCLUDE_LINUX_PARSER_H_
 
 #include <fstream>
 #include <regex>
 #include <string>
 
 namespace LinuxParser {
-// Paths
-const std::string kProcDirectory{"/proc/"};
-const std::string kCmdlineFilename{"/cmdline"};
-const std::string kCpuinfoFilename{"/cpuinfo"};
-const std::string kStatusFilename{"/status"};
-const std::string kStatFilename{"/stat"};
-const std::string kUptimeFilename{"/uptime"};
-const std::string kMeminfoFilename{"/meminfo"};
-const std::string kVersionFilename{"/version"};
-const std::string kOSPath{"/etc/os-release"};
-const std::string kPasswordPath{"/etc/passwd"};
+  // Paths
 
-// System
-float MemoryUtilization();
-long UpTime();
-std::vector<int> Pids();
-int TotalProcesses();
-int RunningProcesses();
-std::string OperatingSystem();
-std::string Kernel();
-float ProcessCpuUtilization(int pid);
-// CPU
-enum CPUStates {
-  kUser_ = 0,
-  kNice_,
-  kSystem_,
-  kIdle_,
-  kIOwait_,
-  kIRQ_,
-  kSoftIRQ_,
-  kSteal_,
-  kGuest_,
-  kGuestNice_
-};
-std::vector<std::string> CpuUtilization();
-long Jiffies();
-long ActiveJiffies();
-long ActiveJiffies(int pid);
-long IdleJiffies();
+  //const std::string kProcDirectory{"/proc/"};
+  constexpr char const* kProcDirectory = "/proc/";
+  constexpr char const* kCmdlineFilename = "/cmdline";
+  constexpr char const* kCpuinfoFilename = "/cpuinfo";
+  constexpr char const* kStatusFilename = "/status";
+  constexpr char const* kStatFilename = "/stat";
+  constexpr char const* kUptimeFilename = "/uptime";
+  constexpr char const* kMeminfoFilename = "/meminfo";
+  constexpr char const* kVersionFilename = "/version";
+  constexpr char const* kOSPath = "/etc/os-release";
+  constexpr char const* kPasswordPath = "/etc/passwd";
+  // System
+  float MemoryUtilization();
+  long UpTime();
+  std::vector<int> Pids();
+  int TotalProcesses();
+  int RunningProcesses();
+  std::string OperatingSystem();
+  std::string Kernel();
+  float ProcessCpuUtilization(int pid);
 
-// Processes
-std::string Command(int pid);
-std::string Ram(int pid);
-int Uid(int pid);
-std::string User(int pid);
-long UpTime(int pid);
+  const std::string kNumbers = "0123456789";
+  const std::string kLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+  // CPU
+  enum CPUStates {
+    kUser_ = 0,
+    kNice_,
+    kSystem_,
+    kIdle_,
+    kIOwait_,
+    kIRQ_,
+    kSoftIRQ_,
+    kSteal_,
+    kGuest_,
+    kGuestNice_
+  };
+  std::vector<std::string> CpuUtilization();
+  long Jiffies();
+  long ActiveJiffies();
+  long ActiveJiffies(int pid);
+  long IdleJiffies();
+
+  // Processes
+  std::string Command(int pid);
+  std::string Ram(int pid);
+  int Uid(int pid);
+  std::string User(int pid);
+  long UpTime(int pid);
 
 
-// Memory
-float getMemoryUtilization();
+  // Memory
+  float getMemoryUtilization();
 
-// Uptime
-long getUpTime();
+  // Uptime
+  long getUpTime();
 };  // namespace LinuxParser
 
-#endif
+#endif  // CPPND_SYSTEM_MONITOR_INCLUDE_LINUX_PARSER_H_
