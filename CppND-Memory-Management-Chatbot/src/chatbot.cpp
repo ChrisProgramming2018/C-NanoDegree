ef707edc2df5275b57531d6f9acf204563507e4e
Christian Leininger <christianleininger42@gmail.com>
2020-03-03 12:37:42 +0100

    clean up

--- CppND-Memory-Management-Chatbot/src/chatbot.cpp
+++ CppND-Memory-Management-Chatbot/src/chatbot.cpp
@@ -37,7 +37,6 @@ ChatBot::~ChatBot() {
   // Attention: wxWidgets used NULL and not nullptr 
   if(_image != NULL) {
     delete _image;
-    _image = NULL;
   }
 }
 
@@ -75,67 +74,53 @@ void ChatBot::ReceiveMessageFromUser(std::string message) {
     _currentNode->MoveChatbotToNewNode(newNode);
 }
 
-void ChatBot::SetCurrentNode(GraphNode *node)
-{
-    // update pointer to current node
-    _currentNode = node;
+// __________________________________________________________________________________________________________________________________________________
+void ChatBot::SetCurrentNode(GraphNode *node) {
+  // update pointer to current node
+  _currentNode = node;
+  
+  // select a random node answer (if several answers should exist)
+  std::vector<std::string> answers = _currentNode->GetAnswers();
+  std::mt19937 generator(int(std::time(0)));
+  std::uniform_int_distribution<int> dis(0, answers.size() - 1);
+  std::string answer = answers.at(dis(generator));
 
-    // select a random node answer (if several answers should exist)
-    std::vector<std::string> answers = _currentNode->GetAnswers();
-    std::mt19937 generator(int(std::time(0)));
-    std::uniform_int_distribution<int> dis(0, answers.size() - 1);
-    std::string answer = answers.at(dis(generator));
-
-    // send selected node answer to user
-    _chatLogic->SendMessageToUser(answer);
+  // send selected node answer to user
+  _chatLogic->SendMessageToUser(answer);
 }
 
-int ChatBot::ComputeLevenshteinDistance(std::string s1, std::string s2)
-{
-    // convert both strings to upper-case before comparing
-    std::transform(s1.begin(), s1.end(), s1.begin(), ::toupper);
-    std::transform(s2.begin(), s2.end(), s2.begin(), ::toupper);
+int ChatBot::ComputeLevenshteinDistance(std::string s1, std::string s2) {
+  // convert both strings to upper-case before comparing
+  std::transform(s1.begin(), s1.end(), s1.begin(), ::toupper);
+  std::transform(s2.begin(), s2.end(), s2.begin(), ::toupper);
 
-    // compute Levenshtein distance measure between both strings
-    const size_t m(s1.size());
-    const size_t n(s2.size());
-
-    if (m == 0)
-        return n;
-    if (n == 0)
-        return m;
-
-    size_t *costs = new size_t[n + 1];
-
-    for (size_t k = 0; k <= n; k++)
-        costs[k] = k;
-
-    size_t i = 0;
-    for (std::string::const_iterator it1 = s1.begin(); it1 != s1.end(); ++it1, ++i)
-    {
-        costs[0] = i + 1;
-        size_t corner = i;
-
-        size_t j = 0;
-        for (std::string::const_iterator it2 = s2.begin(); it2 != s2.end(); ++it2, ++j)
-        {
-            size_t upper = costs[j + 1];
-            if (*it1 == *it2)
-            {
-                costs[j + 1] = corner;
-            }
-            else
-            {
-                size_t t(upper < corner ? upper : corner);
-                costs[j + 1] = (costs[j] < t ? costs[j] : t) + 1;
-            }
-
-            corner = upper;
-        }
+  // compute Levenshtein distance measure between both strings
+  const size_t m(s1.size());
+  const size_t n(s2.size());
+  if (m == 0) {return n;}
+  if (n == 0) {return m;}
+  size_t *costs = new size_t[n + 1];
+  for (size_t k = 0; k <= n; k++) {
+    costs[k] = k;
+  }
+  
+  size_t i = 0;
+  for (std::string::const_iterator it1 = s1.begin(); it1 != s1.end(); ++it1, ++i) {
+    costs[0] = i + 1;
+    size_t corner = i;
+    size_t j = 0;
+    for (std::string::const_iterator it2 = s2.begin(); it2 != s2.end(); ++it2, ++j) {
+      size_t upper = costs[j + 1];
+      if (*it1 == *it2) {
+        costs[j + 1] = corner;
+      } else {
+        size_t t(upper < corner ? upper : corner);
+        costs[j + 1] = (costs[j] < t ? costs[j] : t) + 1;
+      }
+      corner = upper;
     }
-
-    int result = costs[n];
-    delete[] costs;
-
-    return result;
+  }
+  int result = costs[n];
+  delete[] costs;
+  return result;
 }
