46b3a8ebb792a9d05faf6290eba9ee10124aaccf
Christian Leininger <christianleininger42@gmail.com>
2020-03-06 11:26:45 +0100

    checkstyle

--- CppND-Memory-Management-Chatbot/src/graphedge.cpp
+++ CppND-Memory-Management-Chatbot/src/graphedge.cpp
@@ -1,22 +1,28 @@
-#include "graphnode.h"
-#include "graphedge.h"
+// Copyright 2020
+// Udacity Project
+// Author: Christian Leininger <info2016frei@gmail.com>
 
-GraphEdge::GraphEdge(int id)
-{
-    _id = id;
+
+#include <string>
+#include "../include/graphnode.h"
+#include "../include/graphedge.h"
+
+// ________________________________________________________________________________________________
+GraphEdge::GraphEdge(int id) {
+  _id = id;
 }
 
-void GraphEdge::SetChildNode(GraphNode *childNode)
-{
-    _childNode = childNode;
+// ________________________________________________________________________________________________
+void GraphEdge::SetChildNode(GraphNode *childNode) {
+  _childNode = childNode;
 }
 
-void GraphEdge::SetParentNode(GraphNode *parentNode)
-{
-    _parentNode = parentNode;
+// ________________________________________________________________________________________________
+void GraphEdge::SetParentNode(GraphNode *parentNode) {
+  _parentNode = parentNode;
 }
 
-void GraphEdge::AddToken(std::string token)
-{
+// ________________________________________________________________________________________________
+void GraphEdge::AddToken(std::string token) {
     _keywords.push_back(token);
 }
