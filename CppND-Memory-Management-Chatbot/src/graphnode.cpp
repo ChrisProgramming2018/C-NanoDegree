1ecf797c7e0ad3391da272479256f07bfd2a01c7
Christian Leininger <christianleininger42@gmail.com>
2020-03-06 11:28:54 +0100

    Add files via upload

--- CppND-Memory-Management-Chatbot/src/graphnode.cpp
+++ CppND-Memory-Management-Chatbot/src/graphnode.cpp
@@ -1,60 +1,62 @@
-#include "graphedge.h"
-#include "graphnode.h"
+// Copyright 2020
+// Udacity Project
+// Author: Christian Leininger <info2016frei@gmail.com>
 
-GraphNode::GraphNode(int id)
-{
-    _id = id;
+#include <memory>
+#include <utility>
+#include <string>
+#include "../include/graphedge.h"
+#include "../include/graphnode.h"
+
+// ________________________________________________________________________________________________
+GraphNode::GraphNode(int id) {
+  _id = id;
 }
 
-GraphNode::~GraphNode()
-{
-    //// STUDENT CODE
-    ////
+// ________________________________________________________________________________________________
+GraphNode::~GraphNode() {
+  //// STUDENT CODE
+  ////
 
-    delete _chatBot; 
+  // delete _chatBot;
 
-    ////
-    //// EOF STUDENT CODE
+  ////
+  //// EOF STUDENT CODE
 }
 
-void GraphNode::AddToken(std::string token)
-{
-    _answers.push_back(token);
+// ________________________________________________________________________________________________
+void GraphNode::AddToken(std::string token) {
+  _answers.push_back(token);
 }
 
-void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
-{
-    _parentEdges.push_back(edge);
+// ________________________________________________________________________________________________
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge) {
+  _parentEdges.push_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
-{
-    _childEdges.push_back(edge);
+// ________________________________________________________________________________________________
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge) {
+  _childEdges.push_back(std::move(edge));
 }
 
 //// STUDENT CODE
 ////
-void GraphNode::MoveChatbotHere(ChatBot *chatbot)
-{
-    _chatBot = chatbot;
-    _chatBot->SetCurrentNode(this);
+
+// ________________________________________________________________________________________________
+void GraphNode::MoveChatbotHere(ChatBot chatbot) {
+  _chatBot = std::move(chatbot);
+  _chatBot.SetCurrentNode(this);
 }
 
-void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
-{
-    newNode->MoveChatbotHere(_chatBot);
-    _chatBot = nullptr; // invalidate pointer at source
+// ________________________________________________________________________________________________
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode) {
+  newNode->MoveChatbotHere(std::move(_chatBot));
 }
 ////
 //// EOF STUDENT CODE
 
-GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
-{
-    //// STUDENT CODE
-    ////
-
-    return _childEdges[index];
-
-    ////
-    //// EOF STUDENT CODE
+// ________________________________________________________________________________________________
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index) {
+  // the vector stores unique pointer
+  return _childEdges[index].get();
 }
