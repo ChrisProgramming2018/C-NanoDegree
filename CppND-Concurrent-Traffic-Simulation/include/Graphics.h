6d58e6fdc1ad4d75a20a556612e813abd445ef9d
Christian Leininger <christianleininger42@gmail.com>
2020-03-20 16:28:32 +0100

    checkstyle

--- CppND-Concurrent-Traffic-Simulation/include/Graphics.h
+++ CppND-Concurrent-Traffic-Simulation/include/Graphics.h
@@ -1,33 +1,34 @@
-#ifndef GRAPHICS_H
-#define GRAPHICS_H
+// Copyright 2020
+// Udacity Project
+// Author: Christian Leininger <info2016frei@gmail.com>
 
+#ifndef CPPND_CONCURRENT_TRAFFIC_SIMULATION_INCLUDE_GRAPHICS_H_
+#define CPPND_CONCURRENT_TRAFFIC_SIMULATION_INCLUDE_GRAPHICS_H_
+
+#include <opencv2/core.hpp>
 #include <string>
 #include <vector>
-#include <opencv2/core.hpp>
 #include "../include/TrafficObject.h"
 
-class Graphics
-{
-public:
-    // constructor / desctructor
+class Graphics {
+ public:
+  // constructor / desctructor
+  // getters / setters
+  void setBgFilename(std::string filename) { _bgFilename = filename; }
+  void setTrafficObjects(const std::vector<std::shared_ptr<TrafficObject>> &trafficObjects) { _trafficObjects = trafficObjects; }
+  // typical behaviour methods
+  void simulate();
 
-    // getters / setters
-    void setBgFilename(std::string filename) { _bgFilename = filename; }
-    void setTrafficObjects(std::vector<std::shared_ptr<TrafficObject>> &trafficObjects) { _trafficObjects = trafficObjects; };
+ private:
+  // typical behaviour methods
+  void loadBackgroundImg();
+  void drawTrafficObjects();
 
-    // typical behaviour methods
-    void simulate();
-
-private:
-    // typical behaviour methods
-    void loadBackgroundImg();
-    void drawTrafficObjects();
-
-    // member variables
-    std::vector<std::shared_ptr<TrafficObject>> _trafficObjects;
-    std::string _bgFilename;
-    std::string _windowName;
-    std::vector<cv::Mat> _images;
+  // member variables
+  std::vector<std::shared_ptr<TrafficObject>> _trafficObjects;
+  std::string _bgFilename;
+  std::string _windowName;
+  std::vector<cv::Mat> _images;
 };
 
-#endif
+#endif  // CPPND_CONCURRENT_TRAFFIC_SIMULATION_INCLUDE_GRAPHICS_H_
