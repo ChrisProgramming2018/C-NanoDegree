d8ee3d382dfac541a5c597ae90dcf4837194005b
Christian Leininger <christianleininger42@gmail.com>
2020-03-20 19:01:34 +0100

    checkstyle

---
+++ TrafficObject.cpp
@@ -0,0 +1,39 @@
+// Copyright 2020
+// Udacity Project
+// Author: Christian Leininger <info2016frei@gmail.com>
+
+#include <algorithm>
+#include <iostream>
+#include <chrono>
+#include "../include/TrafficObject.h"
+
+// init static variable
+// ________________________________________________________________________________________________
+int TrafficObject::_idCnt = 0;
+
+std::mutex TrafficObject::_mtx;
+// ________________________________________________________________________________________________
+void TrafficObject::setPosition(double x, double y) {
+  _posX = x;
+  _posY = y;
+}
+
+// ________________________________________________________________________________________________
+void TrafficObject::getPosition(double &x, double &y) {
+  x = _posX;
+  y = _posY;
+}
+
+// ________________________________________________________________________________________________
+TrafficObject::TrafficObject() {
+  _type = ObjectType::noObject;
+  _id = _idCnt++;
+}
+
+// ________________________________________________________________________________________________
+TrafficObject::~TrafficObject() {
+  // set up thread barrier before this object is destroyed
+  std::for_each(threads.begin(), threads.end(), [](std::thread &t) {
+      t.join();
+      });
+}
