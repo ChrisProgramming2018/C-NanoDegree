4a083ad499119c367969823fcb4529d758e0c0b7
Christian Leininger <christianleininger42@gmail.com>
2020-03-20 11:42:00 +0100

    data street

--- CppND-Concurrent-Traffic-Simulation/src/Street.cpp
+++ CppND-Concurrent-Traffic-Simulation/src/Street.cpp
@@ -2,27 +2,25 @@
 // Udacity Project
 // Author: Christian Leininger <info2016frei@gmail.com>
 
-#include <memory>
 #include <iostream>
-#include "../include/Vehicle.h"
-#include "../include/Intersection.h"
-#include "../include/Street.h"
+#include "Vehicle.h"
+#include "Intersection.h"
+#include "Street.h"
 
 
 // __________________________________________________________________________________________________________________________________________________
 Street::Street() {
   _type = ObjectType::objectStreet;
-  _length = 1000.0;  // in m
+  _length = 1000.0; // in m
 }
 
 // __________________________________________________________________________________________________________________________________________________
 void Street::setInIntersection(std::shared_ptr<Intersection> in) {
   _interIn = in;
-  in->addStreet(get_shared_this());  // add this street to list of streets connected to the intersection
+  in->addStreet(get_shared_this()); // add this street to list of streets connected to the intersection
 }
-
 // __________________________________________________________________________________________________________________________________________________
 void Street::setOutIntersection(std::shared_ptr<Intersection> out) {
   _interOut = out;
-  out->addStreet(get_shared_this());  // add this street to list of streets connected to the intersection
+  out->addStreet(get_shared_this()); // add this street to list of streets connected to the intersection
 }
