c2ff27cb95598b7a908e1728542422a754c24058
Programmer <info2016frei@gmail.com>
2020-07-31 19:52:04 +0200

    remove old files

--- CppND-Project/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "MyDlg.h"
-#include <iostream>
-int main (int argc, char **argv) {
-  QApplication theApp(argc,argv);
-  MyDlg dlg;
-  dlg.show();
-  return theApp.exec();
-}
